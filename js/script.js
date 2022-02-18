2a115af0ed96c469ede5cd0f17ef5c612597994a
Rootdiv <rootdiv@mail.ru>
2022-02-18 14:00:18 +0300

    feat: mute checkbox and  mobile menu Оperation of the mute checkbox and open and closing the mobile menu

--- js/script.js
+++ js/script.js
@@ -1,22 +1,75 @@
 'use strict';
 
-const sliderThumbs = new Swiper('.slider-thumbs', {
-  loop: true,
-  spaceBetween: 20,
-  slidesPerView: 3,
-  centeredSlides: true,
-  loopedSlides: 4,
+//Бургер
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+  navigation.classList.add('navigation--active');
 });
 
-sliderThumbs.on('click', swiper => {
-  swiper.slideTo(swiper.clickedIndex);
-})
-
-const sliderMain = new Swiper('.slider-main', {
-  loop: true,
-  spaceBetween: 10,
-  loopedSlides: 4,
+navigationClose.addEventListener('click', () => {
+  navigation.classList.remove('navigation--active');
 });
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+//Музыка
+try {
+  const mute = document.querySelector('.mute__checkbox');
+  const audio = new Audio('./audio/waterTower.mp3');
+  //audio.autoplay = true;
+  const checkMute = () => {
+    if (mute.checked) {
+      audio.play().catch(() => {
+        mute.checked = false;
+      });
+    } else {
+      audio.pause();
+    }
+  };
+
+  if (mute) {
+    setTimeout(checkMute, 2000);
+  }
+
+  mute.addEventListener('change', checkMute);
+} catch {
+  console.log('На этой странице нет музыки');
+}
+
+try {
+  const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+    loopedSlides: 4,
+  });
+
+  sliderThumbs.on('click', swiper => {
+    swiper.slideTo(swiper.clickedIndex);
+  });
+
+  const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    spaceBetween: 10,
+    loopedSlides: 4,
+  });
+
+  sliderThumbs.controller.control = sliderMain;
+  sliderMain.controller.control = sliderThumbs;
+  const videos = document.querySelectorAll('video');
+  sliderMain.on('slideChange', () => {
+    for (let i = 0; i < videos.length; i++) {
+      videos[i].pause();
+    }
+  });
+
+  const pagination = document.querySelector('.pagination');
+  const paginationButton = document.querySelector('.pagination__arrow');
+  paginationButton.addEventListener('click', () => {
+    pagination.classList.toggle('pagination--active');
+  });
+} catch {
+  console.log('На этой странице нет слайдера');
+}
